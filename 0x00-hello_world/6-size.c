0591c0dcd32cbb566c463502b3885dfd574428e4
Eyosiyas <eyosiyaseyasu19@gmail.com>
2022-04-10 09:24:19 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,5 +18,5 @@ printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-  return (0);
+return (0);
 }
