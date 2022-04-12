0edd1f96bacd3501a1cfb6d56ff1982f1e5459e2
Eyosiyas <eyosiyaseyasu19@gmail.com>
2022-04-12 12:02:44 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
 int main(void)
 {
-  int fd[2];
-  char buf1[12] = "_putchar";
-  char buf2[12];
+int fd[2];
+char buf1[12] = "_putchar";
+char buf2[12];
 
-  fd[0] = open("bar.txt", O_RDWR);
-  fd[1] = open("bar.txt", O_RDWR);
+fd[0] = open("bar.txt", O_RDWR);
+fd[1] = open("bar.txt", O_RDWR);
 
-  write(fd[0], buf1, strlen(buf1));
-  write(1, buf2, read(fd[1], buf2, 12));
+write(fd[0], buf1, strlen(buf1));
+write(1, buf2, read(fd[1], buf2, 12));
 
-  close(fd[0]);
-  close(fd[1]);
+close(fd[0]);
+close(fd[1]);
 
-  return 0;
+return (0);
 
 }
