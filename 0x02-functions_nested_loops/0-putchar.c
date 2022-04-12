a46acf0ebfa47936122416c73b9c97cde83ddfd6
Eyosiyas <eyosiyaseyasu19@gmail.com>
2022-04-12 12:00:01 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+#include "main.h"
 int main(void)
 {
-fwrite("_putchar.c");
-return 0;
+  int fd[2];
+  char buf1[12] = "_putchar";
+  char buf2[12];
+
+  fd[0] = open("bar.txt", O_RDWR);
+  fd[1] = open("bar.txt", O_RDWR);
+
+  write(fd[0], buf1, strlen(buf1));
+  write(1, buf2, read(fd[1], buf2, 12));
+
+  close(fd[0]);
+  close(fd[1]);
+
+  return 0;
+
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
-#include <cs50.h>
 #include <string.h>
 #include <ctype.h>
+#include <fcntl.h>
+#include <unistd.h>
