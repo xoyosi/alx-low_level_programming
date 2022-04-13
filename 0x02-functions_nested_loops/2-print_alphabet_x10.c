ae3fd20078846210d3369232fa85ad7db573b477
Eyosiyas <eyosiyaseyasu19@gmail.com>
2022-04-13 14:10:13 -0700

    print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,17 @@
 #include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void); void print_alphabet_x10(void)
 {
-  int count = 0;
-  char c;
-
-  while (count++ <= 9)
-    {
-      for (c = 'a'; c <= 'z'; c++)
-	putchar(c);
-      putchar('\n');
-    }
-  
+int count = 0;
+char c;
+while (count++ <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+}
 }
