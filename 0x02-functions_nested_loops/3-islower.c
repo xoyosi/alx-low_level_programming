86146a1bc4067c4da9f2dc9766367b25477224fb
Eyosiyas <eyosiyaseyasu19@gmail.com>
2022-04-13 14:14:57 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void); int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
