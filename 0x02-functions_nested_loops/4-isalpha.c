b273097ef7aa73b3bc4a2effc4e661b7b8d4c280
Eyosiyas <eyosiyaseyasu19@gmail.com>
2022-04-13 14:30:25 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void); int _isalpha(int c)
+{
+if (c >= 'A' && c <= 'z')
+return (1);
+else
+return (0);
+}
