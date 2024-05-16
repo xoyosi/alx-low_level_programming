210198cd8274e637fd39898a9b72647aca12d0a8
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-16 18:27:12 -0400

    argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * @argc: the arg count
+ *
+ * @argv: the arg vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
