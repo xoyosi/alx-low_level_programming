75f9629423e106ec413115fac3d7e82677d4bb95
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-17 17:55:56 -0400

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,16 +12,26 @@
 
 char *create_array(unsigned int size, char c)
 {
+	int i;
+	char *array;
+
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	c = malloc(sizeof(char) * size);
+	*array = (char *)malloc(sizeof(char) * size);
 
-	if (c == NULL)
+	if (array == NULL)
 	{
 		return (NULL)
 	}
+	
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
 	return (c);
 }
