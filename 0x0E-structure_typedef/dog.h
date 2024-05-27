9c28a3816e89944225e3b5ce7e850edc434e3d7c
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-27 19:19:58 -0400

    stuct_typedef

---
+++ 0x0E-structure_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - the structure
+ *
+ * name: the name
+ *
+ * age: the age
+ *
+ * owner: the owner
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
