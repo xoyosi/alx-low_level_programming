40ae1d4e36befcf977156f9b5ca6eb055b06292e
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-27 19:28:34 -0400

    struct_typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - initial the struct
+ *
+ * @d: the object name
+ *
+ * @name: the name
+ *
+ * @age: the age
+ *
+ * @owner: the owner
+ *
+ */
+
+void init_god(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
