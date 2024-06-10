f7ffb5864ce3661a558c612d6fa8c50590d328ee
Eyosiyas <xoyosi19@gmail.com>
2024-06-10 14:06:32 -0700

    struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,10 +9,10 @@
 
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-    {
-        return;
-    }
+	if (d == NULL)
+	{
+		return;
+	}
 
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
