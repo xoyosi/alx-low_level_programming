1ea842339f8f35025a682e230204297b786234b9
Eyosiyas <xoyosi19@gmail.com>
2024-06-13 04:02:07 -0700

    variadic

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * sum_them_all - sum all together
+ *
+ * @n: the first arg ( the elipse count )
+ *
+ * Return: the sum of all
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum = 0;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
