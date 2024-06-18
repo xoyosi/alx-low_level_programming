8d41d8a91b6295ef15948b98c3914da3d75d8a1c
Eyosiyas <xoyosi19@gmail.com>
2024-06-18 15:28:30 -0700

    linked_lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - the length of the list
+ *
+ * @h: the struct
+ *
+ * Return: the length of the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count;
+	int i;
+
+	for (i = 0; h->next != NULL; i++)
+	{
+		count++
+	}
+
+	return (count);
+}
